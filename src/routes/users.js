2bc31ad2e23a05facb6c2e5e5b9aa64b980432ff
oscar-ekeyekwu <oscarekeyekwu@gmail.com>
2020-09-17 11:29:01 +0100

    added signout route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,4 +1,22 @@
 import express from "express";
+import UserController from "../controllers/userController";
+import validationRule from "../middleware/validationRule";
+// import { check } from "express-validator";
+import signupValidation from "../utils/validations/signupValidation";
 const userRouter = express.Router();
 
+//user sign up
+userRouter.post(
+  "/signup",
+  validationRule(signupValidation),
+  UserController.signup
+);
+
+//user sign in
+userRouter.post("/signin", UserController.signin);
+
+//user sign out
+userRouter.get("/signout", UserController.signout);
+
+//export router
 export default userRouter;
